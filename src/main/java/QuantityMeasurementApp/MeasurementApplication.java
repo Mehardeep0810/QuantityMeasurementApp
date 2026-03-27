c9d3d321ffb22a579b52ef24c91437c0f7d909b4
Mehardeep Kaur <mehardeepk.38@gmail.com>
2026-03-27 10:56:56 +0530

    Initial commit - Main

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -7,39 +7,4 @@ import javax.swing.*;
 
 public class MeasurementApplication {
 
-	// Inner class representing Feet measurement
-	static class Feet {
-		private final double value;
-
-
-		public Feet(double value) {
-			this.value = value;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			// Reflexive check
-			if (this == obj) {
-				return true;
-			}
-
-			if (obj == null || getClass() != obj.getClass()) {
-				return false;
-			}
-
-			Feet other = (Feet) obj;
-			return Double.compare(this.value, other.value) == 0;
-		}
-
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(MeasurementApplication.class,args);
-
-		Feet f1 = new Feet(34.5);
-		Feet f2 = new Feet(34.5);
-
-		System.out.println(f1.equals(f2));
-
-	}
 }
