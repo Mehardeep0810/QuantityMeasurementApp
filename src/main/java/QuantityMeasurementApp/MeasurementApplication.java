70c0f1fc1f557c221b145f8f017768ff203dce77
Mehardeep7287 <mehardeep7287.ca22@chitkara.edu.in>
2026-03-27 13:56:37 +0530

    Updated code files

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -33,6 +33,27 @@ public class MeasurementApplication {
 
 	}
 
+	// Inner class representing Inches measurement
+	static class Inches {
+		private final double value;
+
+		public Inches(double value) {
+			this.value = value;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null || getClass() != obj.getClass()) {
+				return false;
+			}
+			Inches other = (Inches) obj;
+			return Double.compare(this.value, other.value) == 0;
+		}
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MeasurementApplication.class,args);
 
@@ -40,6 +61,11 @@ public class MeasurementApplication {
 		Feet f2 = new Feet(34.5);
 
 		System.out.println(f1.equals(f2));
+
+		Inches i1 = new Inches(12.0);
+		Inches i2 = new Inches(12.0);
+
+		System.out.println("Inches comparison: " + i1.equals(i2));
 
 	}
 }
