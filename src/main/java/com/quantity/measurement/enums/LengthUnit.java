2cf4398ed786cf6ea6dc22e2f3c13aae42899494
Mehardeep7287 <mehardeep7287.ca22@chitkara.edu.in>
2026-04-06 10:04:21 +0530

    UC4

--- src/main/java/com/quantity/measurement/enums/LengthUnit.java
+++ src/main/java/com/quantity/measurement/enums/LengthUnit.java
@@ -1,16 +1,23 @@
  package com.quantity.measurement.enums;
 
-public enum LengthUnit {
-    FEET(1.0),          // Base unit
-    INCHES(1.0 / 12.0); // 12 inches = 1 foot
+ public enum LengthUnit {
 
-    private final double toFeetFactor;
+     FEET(1.0),                 // Base unit
+     INCH(1.0 / 12.0),          // 12 inches = 1 foot
+     YARD(3.0),                 // 1 yard = 3 feet
+     CENTIMETERS(0.0328084);    // 1 cm = 0.0328084 feet
 
-    LengthUnit(double toFeetFactor) {
-        this.toFeetFactor = toFeetFactor;
-    }
+     private final double toFeetFactor;
 
-    public double toFeet(double value) {
-        return value * toFeetFactor;
-    }
-}
+     LengthUnit(double toFeetFactor) {
+         this.toFeetFactor = toFeetFactor;
+     }
+
+     public double toFeet(double value) {
+         return value * toFeetFactor;
+     }
+
+     public double fromFeet(double value) {
+         return value / toFeetFactor;
+     }
+ }
--- src/main/java/com/quantity/measurement/model/QuantityLength.java
+++ src/main/java/com/quantity/measurement/model/QuantityLength.java
@@ -3,25 +3,47 @@ package com.quantity.measurement.model;
 import com.quantity.measurement.enums.LengthUnit;
 
 public class QuantityLength {
+
+    private static final double EPSILON = 1e-6;
+
     private final double value;
     private final LengthUnit unit;
 
     public QuantityLength(double value, LengthUnit unit) {
+        if (unit == null) {
+            throw new IllegalArgumentException("Unit shouldn't be null");
+        }
         this.value = value;
         this.unit = unit;
     }
 
-    private double toFeet() {
-        return unit.toFeet(value);
+    public double toConvert(LengthUnit targetUnit) {
+        return convert(this.value, this.unit, targetUnit);
+    }
+
+    public static double convert(double value, LengthUnit sourceUnit, LengthUnit targetUnit) {
+        if (sourceUnit == null || targetUnit == null) {
+            throw new IllegalArgumentException("Units shouldn't be empty!!");
+        }
+        if (!Double.isFinite(value)) {
+            throw new IllegalArgumentException("Invalid numeric value!");
+        }
+        double valueInFeet = sourceUnit.toFeet(value);
+        return targetUnit.fromFeet(valueInFeet);
     }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
-        if (!(obj instanceof QuantityLength)) return false;
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
 
         QuantityLength other = (QuantityLength) obj;
-        return Double.compare(this.toFeet(), other.toFeet()) == 0;
+        double thisInFeet = this.unit.toFeet(this.value);
+        double otherInFeet = other.unit.toFeet(other.value);
+
+        return Math.abs(thisInFeet - otherInFeet) < EPSILON;
     }
 
     @Override
