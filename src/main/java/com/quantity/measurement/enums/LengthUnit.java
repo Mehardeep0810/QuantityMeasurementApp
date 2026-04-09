ca1c377c0d143fdc2570f8ceb463730fa49739bf
Mehardeep7287 <mehardeep7287.ca22@chitkara.edu.in>
2026-04-09 09:21:06 +0530

    UC6

--- src/main/java/com/quantity/measurement/enums/LengthUnit.java
+++ src/main/java/com/quantity/measurement/enums/LengthUnit.java
@@ -2,10 +2,10 @@ package com.quantity.measurement.enums;
 
 public enum LengthUnit {
 
-    FEET(1.0),              // 1 foot = 1 foot
-    INCH(1.0 / 12.0),       // 1 inch = 0.0833 feet
-    YARD(3.0),              // 1 yard = 3 feet
-    CM(0.0328084);          // 1 cm = 0.0328084 feet (≈ 0.393701 inches)
+    FEET(1.0),
+    INCH(1.0 / 12),
+    YARD(3.0),                 // 1 yard = 3 feet
+    CENTIMETERS(0.0328084);     // 1 cm = 0.0328084 feet
 
     private final double toFeetFactor;
 
@@ -16,8 +16,7 @@ public enum LengthUnit {
     public double toFeet(double value) {
         return value * toFeetFactor;
     }
-
-    public double fromFeet(double value) {
+    public double fromFeet(double value){
         return value / toFeetFactor;
     }
 }
