57b982061786d91d4e143dad4be4dc0a78f51ae1
Mehardeep7287 <mehardeep7287.ca22@chitkara.edu.in>
2026-04-21 11:35:08 +0530

    UC9

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -1,24 +1,41 @@
 package QuantityMeasurementApp;
 
 import com.quantity.measurement.enums.LengthUnit;
+import com.quantity.measurement.enums.WeightUnit;
 import com.quantity.measurement.model.QuantityLength;
+import com.quantity.measurement.model.QuantityWeight;
 
 public class MeasurementApplication {
+
 	public static void main(String[] args) {
+
+		// ================= LENGTH =================
 		QuantityLength q1 = new QuantityLength(1, LengthUnit.FEET);
 		QuantityLength q2 = new QuantityLength(12, LengthUnit.INCH);
 
-		System.out.println("Equality check: " + q1.equals(q2));
+		System.out.println(q1 + " and " + q2 + " Equal (" + q1.equals(q2) + ")");
 
 		QuantityLength q3 = new QuantityLength(1, LengthUnit.INCH);
 		QuantityLength q4 = new QuantityLength(1, LengthUnit.INCH);
 
-		System.out.println("Equality check: " + q3.equals(q4));
+		System.out.println(q3 + " and " + q4 + " Equal (" + q3.equals(q4) + ")");
 
-		QuantityLength sum = q1.add(q2);
-		System.out.println("Addition result (feet): " + sum.toConvert(LengthUnit.FEET));
 
-		QuantityLength sumInYards = q1.add(q2, LengthUnit.YARD);
-		System.out.println("Addition result (yards): " + sumInYards.toConvert(LengthUnit.YARD));
+		// ================= UC9: WEIGHT =================
+		QuantityWeight w1 = new QuantityWeight(1, WeightUnit.KILOGRAM);
+		QuantityWeight w2 = new QuantityWeight(1000, WeightUnit.GRAM);
+
+		System.out.println("\nWeight:");
+		System.out.println(w1 + " and " + w2 + " Equal (" + w1.equals(w2) + ")");
+
+		// Conversion
+		System.out.println("1 KG in Gram = " + w1.convert(WeightUnit.GRAM));
+
+		// Addition
+		QuantityWeight w3 = new QuantityWeight(500, WeightUnit.GRAM);
+		System.out.println("1 KG + 500 G = " + w1.add(w3));
+
+		// Category safety
+		System.out.println("1 KG == 1 FEET ? " + w1.equals(q1));
 	}
 }
